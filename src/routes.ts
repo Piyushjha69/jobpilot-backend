a96f077e60e7f1066401cf54d123da9cbe620515
Piyushjha69 <piyushjhatkd@gmail.com>
2026-01-26 00:14:05 +0530

    updated src route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import resumeRouter from './modules/resume/route.js';
 import authRouter from './modules/auth/route.js';
-    
+import applicationRoutes from './modules/applications/route.js';
+
 const router = Router();
 
+router.use('/applications', applicationRoutes);
 router.use('/auth', authRouter);
 router.use('/resume', resumeRouter);
 
