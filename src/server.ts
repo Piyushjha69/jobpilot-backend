1d3307be572db3f0f1f4fead9f6f68af4afb117e
Piyushjha69 <piyushjhatkd@gmail.com>
2026-01-29 17:54:09 +0530

    health end point added

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import 'dotenv/config';
 import app from './app.js';
 import connectDB from './config/db.js';
+import { Router } from 'express';
 
 connectDB();
 
 const PORT = process.env.PORT || 5000 ;
+
+const router = Router();
+
+router.get("/health", ( req, res ) => res.send({
+    message: "Healthy"
+}) );
 
 app.listen(PORT, () =>{
     console.log(`Server running on port ${PORT}`);
